e95d14c06aa00623bc2f736465a12e01b906a0a7
AristezioJunior <57036236+AristezioJunior@users.noreply.github.com>
2020-12-24 10:19:03 -0300

    colocando os dados do banco

--- src/main/java/br/com/sistemahospitalar/test/TestInternacao.java
+++ src/main/java/br/com/sistemahospitalar/test/TestInternacao.java
@@ -1,25 +1,31 @@
 package br.com.sistemahospitalar.test;
 
+import java.util.ArrayList;
+
+import br.com.sistemahospitalar.model.Clinica;
+import br.com.sistemahospitalar.model.Leito;
+import br.com.sistemahospitalar.model.Paciente;
+
 public class TestInternacao {
 
 	public static void main(String[] args) {
 		
-		/*
-		 * Paciente pacienteUm = new Paciente(); pacienteUm.setNome("Aristezio Júnior");
-		 * pacienteUm.setIdade(33);
-		 * 
-		 * Clinica clinicaUm = new Clinica(); clinicaUm.setDescricao("Clinica 1");
-		 * clinicaUm.setLstLeito(new ArrayList<Leito>());
-		 * 
-		 * Leito leitoUm = new Leito(); leitoUm.setDescricao("Leito 1");
-		 * leitoUm.setClinica(clinicaUm);
-		 * 
-		 * System.out.println(pacienteUm.getNome());
-		 * System.out.println(pacienteUm.getIdade());
-		 * System.out.println(clinicaUm.getDescricao());
-		 * System.out.println(clinicaUm.getLstLeito());
-		 * System.out.println(leitoUm.getClinica());
-		 */
+		
+		  Paciente pacienteUm = new Paciente(); pacienteUm.setNome("Aristezio Júnior");
+		  pacienteUm.setIdade(33);
+		  
+		  Clinica clinicaUm = new Clinica(); clinicaUm.setDescricao("Clinica 1");
+		  clinicaUm.setLstLeito(new ArrayList<Leito>());
+		  
+		  Leito leitoUm = new Leito(); leitoUm.setDescricao("Leito 1");
+		  leitoUm.setClinica(clinicaUm);
+		  
+		  System.out.println(pacienteUm.getNome());
+		  System.out.println(pacienteUm.getIdade());
+		  System.out.println(clinicaUm.getDescricao());
+		  System.out.println(clinicaUm.getLstLeito());
+		  System.out.println(leitoUm.getClinica());
+		 
 		
 		
 		
